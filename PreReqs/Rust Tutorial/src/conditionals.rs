d7ee077b5454613bb16946559496a16e1ca65a2d
Shubham Sharma <94298264+Shubharma@users.noreply.github.com>
2023-01-27 00:27:52 +0530

    Update conditionals.rs

--- PreReqs/Rust Tutorial/src/conditionals.rs
+++ PreReqs/Rust Tutorial/src/conditionals.rs
@@ -5,7 +5,6 @@ pub fn run() {
   let check_id: bool = true;
   let knows_person_of_age = true;
 
-  // If/Else
   if age >= 21 && check_id || knows_person_of_age {
     println!("Bartender: What would you like to drink?");
   } else if age < 21 && check_id {
@@ -14,7 +13,6 @@ pub fn run() {
     println!("Bartender: I'll need to see your ID");
   }
 
-  // Shorthand If
   let is_of_age = if age >= 21 { true } else { false };
   println!("Is Of Age: {}", is_of_age)
 }
