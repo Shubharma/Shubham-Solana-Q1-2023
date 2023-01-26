021e33e9f53436bd69a533a02962a0a11041a98e
Shubham Sharma <94298264+Shubharma@users.noreply.github.com>
2023-01-27 00:48:01 +0530

    Update error.rs

--- PreReqs/Solana Escrow/src/error.rs
+++ PreReqs/Solana Escrow/src/error.rs
@@ -1,11 +1,9 @@
-// inside error.rs
 use thiserror::Error;
 
 use solana_program::program_error::ProgramError;
 
 #[derive(Error, Debug, Copy, Clone)]
 pub enum EscrowError {
-    /// Invalid instruction
     #[error("Invalid Instruction")]
     InvalidInstruction,
 }
