42575f9b2f58bdd4b7a59174b542018539df7d98
Shubham Sharma <94298264+Shubharma@users.noreply.github.com>
2023-01-27 00:29:10 +0530

    Update functions.rs

--- PreReqs/Rust Tutorial/src/functions.rs
+++ PreReqs/Rust Tutorial/src/functions.rs
@@ -3,11 +3,9 @@
 pub fn run() {
   greeting("Hello", "Jane");
 
-  // Bind function values to variables
   let get_sum = add(5, 5);
   println!("Sum: {}", get_sum);
 
-  // Closure
   let n3: i32 = 10;
   let add_nums = |n1: i32, n2: i32| n1 + n2 + n3;
   println!("C Sum: {}", add_nums(3, 3));
